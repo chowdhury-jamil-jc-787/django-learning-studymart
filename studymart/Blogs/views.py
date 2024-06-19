cf174b6334bbcbcda77f38eeae38bc05635ec700
jamil chowdhury <17-34747-2@student.aiub.edu>
2024-06-19 21:13:47 +0600

    template folder variable declear commit

--- studymart/Blogs/views.py
+++ studymart/Blogs/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def blogs(request):
-    return render (request, 'blogs/blogs.html')
+    dtl = 'django template language'
+    page = {'what':'Blogs', 'dt': dtl}
+    return render (request, 'blogs/blogs.html', context=page)
